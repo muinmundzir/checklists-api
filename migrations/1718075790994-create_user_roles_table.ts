6f62c642c00b1327e0fd2cc90274e5b3aa71a3c4
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-06-11 11:10:44 +0700

    feat: add user roles table migration

---
+++ migrations/1718075790994-create_user_roles_table.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUserRolesTable1718075790994 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user_roles',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            default: 'uuid_generate_v4()',
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+      true,
+    );
+
+    await queryRunner.query(`INSERT INTO user_roles (name) VALUES ('user')`);
+    await queryRunner.query(`INSERT INTO user_roles (name) VALUES ('driver')`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user_roles');
+  }
+}
