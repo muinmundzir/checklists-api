d5aba9a7201bf93842eaa417cd06ed9f5550526e
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-06-12 12:35:50 +0700

    refactor: modify import path

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 
+import { AppModule } from '@app/app.module';
 import { ResponseInterceptor } from '@app/interceptors/response.interceptor';
 
 async function bootstrap() {
