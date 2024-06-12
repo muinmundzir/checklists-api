b81888c848300ebbf42e81c902dbc6011d55ac29
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-06-12 09:50:52 +0700

    feat: add response interceptor

---
+++ src/interceptors/response.interceptor.ts
@@ -0,0 +1,29 @@
+// response.interceptor.ts
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { ApiResponse } from '@app/types/response.type';
+
+@Injectable()
+export class ResponseInterceptor<T>
+  implements NestInterceptor<T, ApiResponse<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<ApiResponse<T>> {
+    return next.handle().pipe(
+      map((data) => ({
+        data,
+        error: null,
+        meta: {},
+      })),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { ResponseInterceptor } from '@app/interceptors/response.interceptor';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new ResponseInterceptor());
   app.enableCors();
   await app.listen(3000);
 }
