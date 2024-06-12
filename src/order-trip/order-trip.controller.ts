674c142cf6fc417294c78322f5dcf54ef21a5e5b
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-06-12 11:48:49 +0700

    feat: add confirm order trip endpoint

--- src/order-trip/order-trip.controller.ts
+++ src/order-trip/order-trip.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 import { OrderTripService } from '@app/order-trip/order-trip.service';
 import { Role } from '@app/types/role.enum';
 import { Roles } from '@app/decorators/role.decorator';
+import { GetUser } from '@app/decorators/get-user.decorator';
+import { UserCtx } from '@app/types/user-ctx.type';
 
 @Roles(Role.Driver)
 @Controller('order-trips')
@@ -12,5 +14,13 @@ export class OrderTripController {
   @Get('')
   async getAvailableTrips() {
     return await this.orderTripService.getTrips();
+  }
+
+  @Post('/:id')
+  async confirmOrderTrip(
+    @Param() param: { id: string },
+    @GetUser() user: UserCtx,
+  ) {
+    return await this.orderTripService.confirmTrip(param.id, user);
   }
 }
--- src/order-trip/order-trip.service.ts
+++ src/order-trip/order-trip.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { OrderTrip } from '@app/order-trip/order-trip.entity';
 import { Trip } from '@app/trip/trip.entity';
 import { TripStatus } from '@app/types/status.enum';
+import { UserCtx } from '@app/types/user-ctx.type';
 
 @Injectable()
 export class OrderTripService {
@@ -24,6 +25,31 @@ export class OrderTripService {
       });
 
       return trips;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async confirmTrip(tripId: string, ctx: UserCtx) {
+    try {
+      const { sub: driverId } = ctx;
+
+      const trip = await this.tripRepository.findOneById(tripId);
+
+      if (!trip) throw new NotFoundException('Trip not found.');
+
+      const orderTrip = new OrderTrip();
+      orderTrip.tripId = tripId;
+      orderTrip.driverId = driverId;
+      orderTrip.status = TripStatus.Accepted;
+
+      const savedOrderTrip = await this.orderTripRepository.save(orderTrip);
+
+      // update trip status
+      trip.status = TripStatus.Accepted;
+      await this.tripRepository.save(trip);
+
+      return savedOrderTrip;
     } catch (error) {
       throw error;
     }
