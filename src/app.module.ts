7446070b9e54a9e8e0eda27385e8c2e5fc7ce433
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-06-12 12:35:23 +0700

    refactor: modify import path

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import typeorm from './config/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { UserRoleModule } from './user-role/user-role.module';
-import { AuthModule } from './auth/auth.module';
-import { TripModule } from './trip/trip.module';
-import { OrderTripModule } from './order-trip/order-trip.module';
+import typeorm from '@app/config/typeorm';
+import { AppController } from '@app/app.controller';
+import { AppService } from '@app/app.service';
+import { UserModule } from '@app/user/user.module';
+import { UserRoleModule } from '@app/user-role/user-role.module';
+import { AuthModule } from '@app/auth/auth.module';
+import { TripModule } from '@app/trip/trip.module';
+import { OrderTripModule } from '@app/order-trip/order-trip.module';
 
 @Module({
   imports: [
