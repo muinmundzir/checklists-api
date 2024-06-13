ff097bc0a8ce47b7f04c15f4491579d78df03602
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-06-13 16:57:13 +0700

    feat: add custom exception filter

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from '@app/app.module';
 import { swaggerConfig } from '@app/config/swagger.config';
 import { ResponseInterceptor } from '@app/interceptors/response.interceptor';
+import { HttpExceptionFilter } from '@app/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api-docs', app, document);
