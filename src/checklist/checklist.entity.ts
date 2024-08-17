f8f9748cf75a5dc079099ae17d1cea7278dc3e28
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-08-17 15:42:04 +0700

    feat: add checklist module

---
+++ src/checklist/checklist.entity.ts
@@ -0,0 +1,50 @@
+import { ChecklistItem } from '@app/checklist-item/checklist-item.entity';
+import { User } from '@app/user/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('checklists')
+export class Checklist {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column({ name: 'header_url' })
+  headerUrl: string;
+
+  @Column()
+  description: string;
+
+  @Column({ name: 'user_id' })
+  userId: string;
+
+  @Column({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @Column({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.checklists)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @OneToMany(() => ChecklistItem, (checklistItem) => checklistItem.checklist)
+  items: ChecklistItem[];
+}
