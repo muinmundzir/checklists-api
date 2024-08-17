ae457767eb4ea9a5c8e84300f24c787b4c70cc5e
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-08-17 15:42:13 +0700

    db: create checklist-items migration file

---
+++ migrations/1723882166789-create-checklist-items-table.ts
@@ -0,0 +1,73 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateChecklistItemsTable1723882166789
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'checklist_items',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            default: 'uuid_generate_v4()',
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'content',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'is_completed',
+            type: 'boolean',
+          },
+          {
+            name: 'checklist_id',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+      true,
+    );
+
+    await queryRunner.createForeignKey(
+      'checklist_items',
+      new TableForeignKey({
+        columnNames: ['checklist_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'checklists',
+        onDelete: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('checklist_items');
+  }
+}
