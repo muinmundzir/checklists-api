5d60ff6534aef4549ca48e04ed1a4f7701d37192
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-08-17 15:40:20 +0700

    refactor: remove unused files

--- src/order-trip/dto/trip.param.ts
+++
@@ -1,6 +0,0 @@
-import { IsUUID } from 'class-validator';
-
-export class TripParam {
-  @IsUUID()
-  id: string;
-}
--- src/trip/dto/user.param.ts
+++
@@ -1,6 +0,0 @@
-import { IsUUID } from 'class-validator';
-
-export class UserParam {
-  @IsUUID()
-  id: string;
-}
