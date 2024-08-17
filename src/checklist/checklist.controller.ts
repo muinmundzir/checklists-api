4e641925ef50d889b9f5c19112bab610f27db918
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-08-17 16:48:15 +0700

    feat: add find all user's checklists

--- src/checklist/checklist.controller.ts
+++ src/checklist/checklist.controller.ts
@@ -1,19 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ChecklistService } from './checklist.service';
-import { ApiCreatedResponse, ApiBody, ApiTags } from '@nestjs/swagger';
-import { CreateChecklist } from './dto/create-checklist.dto';
-import { GetUser } from '@app/decorators/get-user.decorator';
-import { UserCtx } from '@app/types/user-ctx.type';
-import { Roles } from '@app/decorators/role.decorator';
-import { Role } from '@app/types/role.enum';
+import { Body, Controller, Post } from '@nestjs/common'
+import { ChecklistService } from './checklist.service'
+import { ApiCreatedResponse, ApiBody, ApiTags } from '@nestjs/swagger'
+import { CreateChecklist } from './dto/create-checklist.dto'
+import { GetUser } from '@app/decorators/get-user.decorator'
+import { UserCtx } from '@app/types/user-ctx.type'
+import { Roles } from '@app/decorators/role.decorator'
+import { Role } from '@app/types/role.enum'
 
+@Roles(Role.User)
 @ApiTags('Checklists')
 @Controller('checklist')
 export class ChecklistController {
   constructor(private readonly checklistService: ChecklistService) {}
 
-  @Roles(Role.User)
   @Post('/')
+  @ApiCreatedResponse({
+    description: 'Return list checklists',
+  })
+  async find(@GetUser() user: UserCtx) {
+    return await this.checklistService.find(user.sub);
+  }
+
+  @Post('/create')
   @ApiCreatedResponse({
     description: 'Return succesfully created checklist',
   })
--- src/checklist/checklist.service.ts
+++ src/checklist/checklist.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
 
-import { ChecklistItemService } from '@app/checklist-item/checklist-item.service';
-import { Checklist } from '@app/checklist/checklist.entity';
-import { CreateChecklist } from '@app/checklist/dto/create-checklist.dto';
+import { ChecklistItemService } from '@app/checklist-item/checklist-item.service'
+import { Checklist } from '@app/checklist/checklist.entity'
+import { CreateChecklist } from '@app/checklist/dto/create-checklist.dto'
 
 @Injectable()
 export class ChecklistService {
@@ -14,31 +14,48 @@ export class ChecklistService {
     private itemsService: ChecklistItemService,
   ) {}
 
+  async find(userId: string) {
+    try {
+      const checklists = await this.checklistRepository.find({
+        relations: ['items'],
+        where: {
+          userId,
+        },
+      })
+
+      return {
+        data: checklists,
+      }
+    } catch (error) {
+      throw error
+    }
+  }
+
   async create(checklistDto: CreateChecklist, userId: string) {
     try {
-      const { title, headerUrl, items } = checklistDto;
+      const { title, headerUrl, items } = checklistDto
 
-      const checklist = new Checklist();
-      checklist.title = title;
-      checklist.headerUrl = headerUrl;
-      checklist.userId = userId;
+      const checklist = new Checklist()
+      checklist.title = title
+      checklist.headerUrl = headerUrl
+      checklist.userId = userId
 
-      const savedChecklist = await this.checklistRepository.save(checklist);
+      const savedChecklist = await this.checklistRepository.save(checklist)
 
-      await this.itemsService.create(items, savedChecklist.id);
+      await this.itemsService.create(items, savedChecklist.id)
 
       const data = await this.checklistRepository.findOne({
         relations: ['items'],
         where: {
           id: savedChecklist.id,
         },
-      });
+      })
 
       return {
         data,
-      };
+      }
     } catch (error) {
-      throw error;
+      throw error
     }
   }
 }
